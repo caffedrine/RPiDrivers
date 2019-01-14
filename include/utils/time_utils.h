adf484757ce6d4ea621cc05f3590e7a6e240c741
caffedrine <bacchusstyle@yahoo.com>
2019-01-14 23:00:36 +0200

    Logger function added!

---
+++ include/utils/time_utils.h
@@ -0,0 +1,97 @@
+#ifndef TIME_UTILS_H
+#define TIME_UTILS_H
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <mutex>
+#include <condition_variable>
+
+namespace TimeUtils
+{
+/* Get time stamp in milliseconds */
+static inline uint64_t millis()
+{
+	return (uint64_t)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+/* Get time stamp in microseconds */
+static inline uint64_t micros()
+{
+	return (uint64_t)std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+/* Get time stamp in nanoseconds */
+static inline uint64_t nanos()
+{
+	return (uint64_t)std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+/* Current timestamp for logging */
+static std::string formatted_time_now()
+{
+	using namespace std::chrono;
+	
+	// get current time
+	auto now = system_clock::now();
+	
+	// get number of milliseconds for the current second
+	// (remainder after division into seconds)
+	auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+	
+	// convert to std::time_t in order to convert to std::tm (broken time)
+	auto timer = system_clock::to_time_t(now);
+	
+	// convert to broken time
+	std::tm bt = *std::localtime(&timer);
+	
+	std::ostringstream oss;
+	
+	oss << std::put_time(&bt, "%H:%M:%S"); // HH:MM:SS
+	oss << '.' << std::setfill('0') << std::setw(3) << ms.count();
+	
+	return oss.str();
+}
+
+class Timer
+{
+public:
+	explicit Timer() = default;
+	
+	uint64_t ElapsedMs()
+	{
+		return ( millis() - (PreviousUs/1000) );
+	}
+	
+	uint64_t ElapsedUs()
+	{	
+		std::unique_lock<std::mutex> mlock(mutex_);
+		uint64_t tmp = PreviousUs;
+		mlock.unlock();     // unlock before notificiation to minimize mutex con
+		cond_.notify_one(); // notify one waiting thread
+		return ( micros() - tmp );		
+	}
+	
+	void Restart()
+	{
+		std::unique_lock<std::mutex> mlock(mutex_);
+		{
+			this->PreviousUs = micros();
+		}
+		mlock.unlock();     // unlock before notificiation to minimize mutex context
+		cond_.notify_one(); // notify one waiting thread
+	}
+	
+private:
+	/* Thread safe read/write positions */
+	std::mutex mutex_;
+	std::condition_variable cond_;
+
+	uint64_t PreviousUs = 0;
+};
+
+}
+
+#endif //TIME_UTILS_H
--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void setup_logger()
 	try
 	{
 		auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt >();
-		auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs.log", 1024*1024*10, 3);
+		auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs.log", 1024*1024*20, 10);
 		
 		logger = new spdlog::logger("Logger", {stdout_sink, rotating_sink});
 		logger->set_level(spdlog::level::debug);
@@ -30,21 +30,17 @@ int main()
 {
 	setup_logger();
 	
-	logger->info("Infooo!");
-	
-	return 1;
-	
-//	logger->debug("This is a debug messge!");
-//	logger->info("Started!");
-//	try
-//	{
-//		I2cMaster i2c("/dev/i2c-0");
-//		i2c.StartSession(0x1234);
-//		uint8_t payload[] = {0x61, 0x62, 0x63, 0x64};
-//		i2c.Write(payload, sizeof(payload) / sizeof(uint8_t));
-//	}
-//	catch(Exception &e)
-//	{
-//		logger->error(e.what());
-//	}
+	logger->debug("This is a debug messge!");
+	logger->info("Started!");
+	try
+	{
+		I2cMaster i2c("/dev/i2c-0");
+		i2c.StartSession(0x1234);
+		uint8_t payload[] = {0x61, 0x62, 0x63, 0x64};
+		i2c.Write(payload, sizeof(payload) / sizeof(uint8_t));
+	}
+	catch(Exception &e)
+	{
+		logger->error(e.what());
+	}
 }
