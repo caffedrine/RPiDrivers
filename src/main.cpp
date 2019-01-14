e07d7194c48fd51b4cd7f3ef02a5a764090b08b8
caffedrine <bacchusstyle@yahoo.com>
2019-01-14 22:52:08 +0200

    Logger function added!

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,50 @@
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/base_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
 #include <drivers/Hal.h>
 #include <drivers/I2C/I2C.h>
+#include <iostream>
 
 /* Logger instance - multi-threaded */
-auto console = spdlog::stdout_color_mt("console");
-
-int main()
+spdlog::logger *logger;
+void setup_logger()
 {
 	try
 	{
-		I2cMaster i2c("/dev/i2c-0");
-		i2c.StartSession(0x1234);
-		uint8_t payload[] = {0x61, 0x62, 0x63, 0x64};
-		i2c.Write(payload, sizeof(payload) / sizeof(uint8_t));
+		auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt >();
+		auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs.log", 1024*1024*10, 3);
+		
+		logger = new spdlog::logger("Logger", {stdout_sink, rotating_sink});
+		logger->set_level(spdlog::level::debug);
 	}
-	catch(Exception &e)
+	catch (const spdlog::spdlog_ex& ex)
 	{
-		console->error(e.what());
+		std::cout << "Log initialization failed: " << ex.what() << std::endl;
 	}
+}
+
+int main()
+{
+	setup_logger();
+	
+	logger->info("Infooo!");
+	
+	return 1;
+	
+//	logger->debug("This is a debug messge!");
+//	logger->info("Started!");
+//	try
+//	{
+//		I2cMaster i2c("/dev/i2c-0");
+//		i2c.StartSession(0x1234);
+//		uint8_t payload[] = {0x61, 0x62, 0x63, 0x64};
+//		i2c.Write(payload, sizeof(payload) / sizeof(uint8_t));
+//	}
+//	catch(Exception &e)
+//	{
+//		logger->error(e.what());
+//	}
 }
